6bb0f7ed73966773248e7f7ef62d76a13e35ca3d
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-03 00:54:01 -0500

    part one complete

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -6,5 +6,9 @@ export const useLocalStorage = (key, initialValue) => {
        return item ? JSON.parse(item) : initialValue;
     });
     
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }
     return [storedValue]
 };
